73b8ce632f57d214b77b95ae16d0d52e7e4ea3f3
Mamoutou02 <sangaremoutou.360@.com>
2025-09-28 20:32:05 +0000

    profil ok sa manque juste le aindpoint

--- src/app/profil/profil.component.ts
+++ src/app/profil/profil.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TabBarComponent } from '../tab-bar/tab-bar.component';
 
 @Component({
@@ -8,11 +9,37 @@ import { TabBarComponent } from '../tab-bar/tab-bar.component';
   templateUrl: './profil.component.html',
   styleUrls: ['./profil.component.scss'],
   standalone: true,
-  imports: [CommonModule, IonicModule,TabBarComponent] // <-- ajoute IonicModule ici
+  imports: [CommonModule, IonicModule, HttpClientModule, TabBarComponent]
 })
 export class ProfilComponent implements OnInit {
 
-  constructor() { }
+  profilData: any = null;
 
-  ngOnInit() {}
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getProfil();
+  }
+
+  getProfil() {
+    // Remplace par ton URL réelle
+    this.http.get('http://localhost:3000/profil').subscribe(
+      (res: any) => {
+        this.profilData = res;
+      },
+      (err) => {
+        console.error('Erreur lors de la récupération du profil', err);
+      }
+    );
+  }
+
+  // Générer les initiales (première et dernière lettre du prénom et nom)
+  getInitiales(): string {
+    if (!this.profilData) return '';
+    const nom = this.profilData.nom || '';
+    const prenom = this.profilData.prenom || '';
+    const nomInit = nom ? nom[0] : '';
+    const prenomInit = prenom ? prenom[0] : '';
+    return `${prenomInit.toUpperCase()}${nomInit.toUpperCase()}`;
+  }
 }
