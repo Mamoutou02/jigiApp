baf5d032823dc691fec4db3695cbd22c3b28e7da
Mamoutou02 <sangaremoutou.360@.com>
2025-09-28 20:09:51 +0000

    profil static ok

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,18 +1,28 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { TabBarComponent } from '../tab-bar/tab-bar.component'; 
+
+// ✅ Import des composants Ionic nécessaires
+import { IonContent, IonButton, IonIcon, IonCard } from '@ionic/angular/standalone';
+
+import { TabBarComponent } from '../tab-bar/tab-bar.component';
+
 @Component({
   selector: 'app-accueil',
   standalone: true,
   templateUrl: './accueil.component.html',
   styleUrls: ['./accueil.component.scss'],
- 
-  schemas: [CUSTOM_ELEMENTS_SCHEMA], 
-  // CORRECTION: Utilisation de l'importation réelle du composant
-  imports: [IonicModule, CommonModule, RouterModule, TabBarComponent], 
-})
-export class AccueilComponent {
+  imports: [
+    CommonModule,
+    RouterModule,
 
-}
+    // ⬇️ Composants Ionic utilisés dans ton HTML
+    IonContent,   // <ion-content>
+    IonButton,    // <ion-button>
+    IonIcon,      // <ion-icon>
+    IonCard,      // <ion-card>
+
+    TabBarComponent, // ton tab-bar
+  ],
+})
+export class AccueilComponent {}
--- src/app/profil/profil.component.ts
+++ src/app/profil/profil.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { TabBarComponent } from '../tab-bar/tab-bar.component';
 
 @Component({
   selector: 'app-profil',
   templateUrl: './profil.component.html',
   styleUrls: ['./profil.component.scss'],
+  standalone: true,
+  imports: [CommonModule, IonicModule,TabBarComponent] // <-- ajoute IonicModule ici
 })
-export class ProfilComponent  implements OnInit {
+export class ProfilComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {}
-
 }
