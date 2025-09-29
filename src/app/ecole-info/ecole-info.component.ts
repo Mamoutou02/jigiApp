a1a9bc038bea3ae696113a715c01bbdb3a5e9f1c
Mamoutou02 <sangaremoutou.360@.com>
2025-09-29 15:58:31 +0000

    espace parents ok logique ok

--- src/app/ecole-info/ecole-info.component.ts
+++ src/app/ecole-info/ecole-info.component.ts
@@ -1,14 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { TabBarComponent } from '../tab-bar/tab-bar.component';
 
 @Component({
   selector: 'app-ecole-info',
   templateUrl: './ecole-info.component.html',
   styleUrls: ['./ecole-info.component.scss'],
+  standalone: true,
+  imports: [CommonModule, IonicModule, FormsModule, HttpClientModule,TabBarComponent]
 })
-export class EcoleInfoComponent  implements OnInit {
+export class EcoleInfoComponent {
 
-  constructor() { }
+  info = {
+    date_debut: '',
+    date_fin: '',
+    raison: '',
+    detaille: ''
+  };
 
-  ngOnInit() {}
+  loading = false;
+
+  constructor(private http: HttpClient) {}
+
+  envoyerInfo() {
+    if (!this.info.date_debut || !this.info.date_fin || !this.info.raison) {
+      alert('Veuillez remplir tous les champs obligatoires');
+      return;
+    }
+
+    this.loading = true;
+
+    this.http.post('http://localhost:3000/ecole-info', this.info)
+      .subscribe({
+        next: (res) => {
+          alert('Informations envoyées avec succès !');
+          this.loading = false;
+          this.info = { date_debut: '', date_fin: '', raison: '', detaille: '' };
+        },
+        error: (err) => {
+          console.error('Erreur API', err);
+          alert('Erreur lors de l’envoi');
+          this.loading = false;
+        }
+      });
+  }
 
 }
