d70b48e269f40643fc4c2fafced6fc87f43d0807
Mamoutou02 <sangaremoutou.360@.com>
2025-09-29 10:21:24 +0000

    page rapport logique ok

--- src/app/list-presence/list-presence.component.ts
+++ src/app/list-presence/list-presence.component.ts
@@ -3,18 +3,45 @@ import { TabBarComponent } from '../tab-bar/tab-bar.component';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-list-presence',
   templateUrl: './list-presence.component.html',
   styleUrls: ['./list-presence.component.scss'],
   standalone: true,
-  imports: [CommonModule, IonicModule,TabBarComponent,RouterModule]
+  imports: [CommonModule, IonicModule, TabBarComponent, RouterModule, HttpClientModule]
 })
-export class ListPresenceComponent  implements OnInit {
+export class ListPresenceComponent implements OnInit {
+  presences: any[] = [];
+  showPresence = false;
+  loading = false;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {}
 
+  togglePresences() {
+    if (this.showPresence) {
+      // masquer la liste
+      this.showPresence = false;
+      this.presences = [];
+      return;
+    }
+
+    // sinon charger la liste
+    this.loading = true;
+    this.http.get<any[]>(`http://localhost:3000/presences`)
+      .subscribe({
+        next: (res) => {
+          this.presences = res;
+          this.showPresence = true;
+          this.loading = false;
+        },
+        error: (err) => {
+          console.error('Erreur API', err);
+          this.loading = false;
+        }
+      });
+  }
 }
--- src/app/rapport/rapport.component.ts
+++ src/app/rapport/rapport.component.ts
@@ -3,15 +3,55 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { TabBarComponent } from '../tab-bar/tab-bar.component';
 import { RouterModule } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-rapport',
   templateUrl: './rapport.component.html',
   styleUrls: ['./rapport.component.scss'],
   standalone: true,
-  imports: [CommonModule, IonicModule,TabBarComponent,RouterModule] 
+  imports: [
+    CommonModule,
+    IonicModule,
+    TabBarComponent,
+    RouterModule,
+    HttpClientModule
+  ]
 })
 export class RapportComponent implements OnInit {
-  constructor() { }
+  bulletins: any[] = [];
+  loading = false;
+  showBulletins = false; // toggle d’affichage
+
+  constructor(private http: HttpClient) {}
+
   ngOnInit() {}
+
+  toggleBulletins() {
+    if (this.showBulletins) {
+      // si déjà visible → cacher
+      this.showBulletins = false;
+      this.bulletins = [];
+    } else {
+      // sinon → charger depuis l’API
+      this.getBulletins();
+    }
+  }
+
+  private getBulletins() {
+    this.loading = true;
+
+    this.http.get<any[]>(`http://localhost:3000/bulletins`)
+      .subscribe({
+        next: (res) => {
+          this.bulletins = res;
+          this.showBulletins = true;
+          this.loading = false;
+        },
+        error: (err) => {
+          console.error('Erreur API', err);
+          this.loading = false;
+        }
+      });
+  }
 }
